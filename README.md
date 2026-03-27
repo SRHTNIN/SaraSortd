04bf4f78d1ecb7870b99a8eb15bda9d39a2b4ecc
Sarahtonin <SRHTNIN@proton.me>
2026-03-27 11:09:15 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Sarasort
 A program that can automatically sort your files to your liking without using AI.
+
+## Dependencies
+toml: Use ```pip install toml``` to install toml.
---
+++ Sarasort.py
@@ -0,0 +1,33 @@
+import toml, os
+
+Conf = None
+
+def Clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def LoadConf():
+    global Conf
+
+    LocalConf = toml.load("Config.toml")
+    Conf = LocalConf
+
+
+def Init():
+    global Conf
+    
+    LoadConf()
+    if (Conf != None):
+        Main()
+
+
+def Main():
+    global Conf
+
+    Clear()
+    print(Conf)
+
+
+Init()
